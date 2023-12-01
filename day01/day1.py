4be898f351bf089efe8df593dcec0fa7dcb3b611
Mihkel Tiganik <mtiganik@gmail.com>
2023-12-01 07:34:49 +0200

    first day

--- day01/day1.py
+++ day01/day1.py
@@ -1,17 +1,31 @@
 f = open("input.txt")
 
-currentSum = 0
-# max = 0
-foods = []
-for x in f:
-  if(x == '\n'):
-    foods.append(currentSum)
+def isNumber(char):
+  if ord(char) >= 48 and ord(char)<= 57:
+    return True
+  return False
 
-    currentSum = 0
-  else: 
-    currentSum += int(x)
-foods.sort(reverse = True)
-foods = foods[:3]
-print("Part1:", foods[0])
-print("Part2:", sum(foods))
+firstDigit = 0
+secondDigit = 0
+
+sum = 0
+currentNum =0
+for x in f:
+  for i in x:
+    if isNumber(i):
+      firstDigit = int(i)
+      break
+  x = x[::-1]
+  for i in x:
+    if isNumber(i):
+      secondDigit = int(i)
+      break
+  
+  currentNum = 10*firstDigit + secondDigit
+  print(currentNum)
+  sum += currentNum
+        
+
+print("Part1:", sum)
+# print("Part2:", sum(foods))
 
---
+++ day01/part2.py
@@ -0,0 +1,62 @@
+f = open("input.txt")
+
+def isNumber(char):
+  if ord(char) >= 48 and ord(char)<= 57:
+    return True
+  return False
+
+def isWrittenNumber(string):
+  if string.startswith("one"): return 1
+  if string.startswith("two"): return 2
+  if string.startswith("three"): return 3
+  if string.startswith("four"): return 4
+  if string.startswith("five"): return 5
+  if string.startswith("six"): return 6
+  if string.startswith("seven"): return 7
+  if string.startswith("eight"): return 8
+  if string.startswith("nine"): return 9
+  return -1
+
+def isWrittenNumberReverse(string):
+  if string.startswith("eno"): return 1
+  if string.startswith("owt"): return 2
+  if string.startswith("eerht"): return 3
+  if string.startswith("ruof"): return 4
+  if string.startswith("evif"): return 5
+  if string.startswith("xis"): return 6
+  if string.startswith("neves"): return 7
+  if string.startswith("thgie"): return 8
+  if string.startswith("enin"): return 9
+  return -1
+
+firstDigit = 0
+secondDigit = 0
+
+sum = 0
+currentNum =0
+for x in f:
+  for i,c in enumerate(x):
+    if isNumber(c):
+      firstDigit = int(c)
+      break
+    if isWrittenNumber(x[i::]) != -1:
+      firstDigit = isWrittenNumber(x[i::])
+      break
+
+  x = x[::-1]
+  for i,c in enumerate(x):
+    if isNumber(c):
+      secondDigit = int(c)
+      break
+    if isWrittenNumberReverse(x[i::]) != -1:
+      secondDigit = isWrittenNumberReverse(x[i::])
+      break
+
+  currentNum = 10*firstDigit + secondDigit
+  print(currentNum)
+  sum += currentNum
+        
+
+print("Part1:", sum)
+# print("Part2:", sum(foods))
+
