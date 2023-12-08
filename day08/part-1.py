12df0d4f50e334eaf87986f01e76dfcf1dd62cc5
Mihkel <mtiganik@gmail.com>
2023-12-08 11:09:38 +0200

    part 1 refactor

--- day08/part-1.py
+++ day08/part-1.py
@@ -1,6 +1,5 @@
 
 f = open("input.txt").read().replace("= (", "").replace(",","").replace(")","").splitlines()
-
 instructions = f[0]
 f = f[2::]
 data = []
@@ -8,20 +7,25 @@ for x in f:
     el = x.split(" ")
     data.append(el)
 idx = 0
-currElement = data[0]
+
+currElement = currElement = [el for el in data if el[0] == "AAA" ][0]
 newElement = ""
 instrLen = len(instructions)
+instructionNo = 0
 while True:
-    dir = instructions[idx%instrLen]
+    dir = instructions[instructionNo]
+    instructionNo += 1
+    if instructionNo >= instrLen:
+        instructionNo = 0
     idx += 1
-    if(idx%1000000 == 0):
-        print(idx)
     if dir == "R":
         newElement = currElement[2]
     else:
         newElement = currElement[1]
-    if newElement == "ZZZ":
+    if newElement == 'ZZZ':
         break        
     currElement = next(x for x in data if x[0] == newElement)
 
+# My time and rank:
+# 02:18:56  16903
 print(idx)
