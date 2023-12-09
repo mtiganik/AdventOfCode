d658d01c1dffc916839f927c8f0871d23aafb9dc
Mihkel Tiganik <mtiganik@gmail.com>
2023-12-09 07:48:10 +0200

    day 9 completed

--- day09/main.py
+++ day09/main.py
@@ -4,7 +4,8 @@ for x in f:
   x = x.split()
   data.append([int(var) for var in x])
 
-valsList = []
+part1Vals = []
+part2Vals = []
 for x in data:
   enumerateList = []
   enumerateList.append(x)
@@ -16,11 +17,16 @@ for x in data:
     x = listToAppend
     enumerateList.append(listToAppend)
     if all(v == 0 for v in listToAppend):
-      wantedVal = 0
+      p1v = 0
+      p2v = 0
       for idx in range(len(enumerateList) -1, 0, -1):
-        wantedVal +=  enumerateList[idx-1][-1]
-      valsList.append(wantedVal)
+        p1v +=  enumerateList[idx-1][-1]
+        p2v =  enumerateList[idx-1][0] - p2v
+      part1Vals.append(p1v)
+      part2Vals.append(p2v)
       break
     listToAppend = []
 
-print(sum(valsList))
+# My time and rank in comments:
+print("Part 1: ", sum(part1Vals)) # 00:33:07   6265
+print("Part 2: ", sum(part2Vals)) # 00:40:11   5982
