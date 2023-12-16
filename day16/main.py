2abac2220dfe470b1e02a485a5a3532bf30e719c
Mihkel Tiganik <mtiganik@gmail.com>
2023-12-16 09:25:44 +0200

    day16 debug

--- day16/main.py
+++ day16/main.py
@@ -3,54 +3,72 @@ f = open("input.txt").read().splitlines()
 data= [list(var) for var in f]
 tData = []
 for x in data:
-  line = [False for _ in x]
+  line = [0 for _ in x]
   tData.append(line)
 
-def move(i,j,dir,lim):
-  if i < 0 or i > len(data[0]) or j < 0 or j > len(data):
+prims = [2,3,5,7,11]
+
+# move(0,0,2)
+
+beams = [[0,0,2]]
+def move(i,j,dir):
+
+  if i < 0 or i > len(data[0])-1 or j < 0 or j > len(data)-1:
     return
-  lim += 1
-  if lim > 200:
-    return
-  tData[i][j] = True
+
+  cellVal = tData[i][j]
+  if cellVal == 0:
+    tData[i][j] = prims[dir]
+  else:
+    mod = cellVal % prims[dir]
+    if mod != 0:
+      tData[i][j] *= prims[dir]
+    else: return
   
   if data[i][j] == ".":
-    if dir == 2: i = i+1
-    elif dir == 4: i = i-1
-    if dir == 1: j = j+1
-    elif dir == 4: j = j-1
-    move(i,j,dir,lim)
+    if dir == 1:   return move(i-1,j, dir)
+    elif dir == 2: return move(i,j+1, dir)
+    elif dir == 3: return move(i+1,j, dir)
+    elif dir == 4: return move(i,j-1, dir)
+    
   elif data[i][j] == '|':
     if dir in [2,4]:
-      move(i-1,j, 1,lim)
-      move(i+1,j, 3,lim)
+      move(i-1,j, 1)
+      move(i+1,j, 3)
     else:
-      if dir == 1: j = j+1
-      else: j = j-1
-      move(i,j,dir,lim)
+      if dir == 1: i = i-1
+      else: i = i+1
+      return move(i,j,dir)
   elif data[i][j] == '-':
     if dir in [1,3]:
-      move(i,j-1, 4,lim)
-      move(i,j+1, 2,lim)
+      move(i,j-1, 4)
+      move(i,j+1, 2)
     else:
-      if dir == 2: move(i+1,j,dir,lim)
-      else: move(i-1, dir,lim)
+      if dir == 2: return move(i,j+1,dir)
+      else: return move(i,j-1, dir)
   elif data[i][j] == '\\':
-    if dir == 1: move(i,j-1, 4,lim)
-    elif dir == 2: move(i+1,j,3,lim)
-    elif dir == 3: move(i,j+1, 2,lim)
-    elif dir == 4: move(i-1,j,1,lim)
+    if dir == 1: return move(i,j-1, 4)
+    elif dir == 2: return move(i+1,j,3)
+    elif dir == 3: return move(i,j+1, 2)
+    elif dir == 4: return move(i-1,j,1)
   elif data[i][j] == '/':
-    if dir == 1:   move(i,j+1,2,lim)
-    elif dir == 2: move(i-1,j,1,lim)
-    elif dir == 3: move(i,j-1,4,lim)
-    elif dir == 4: move(i+1,j,3,lim)
+    if dir == 1:   return move(i,j+1,2)
+    elif dir == 2: return move(i-1,j,1)
+    elif dir == 3: return move(i,j-1,4)
+    elif dir == 4: return move(i+1,j,3)
 
-# 1-north
-# 2-east
-# 3-south
-# 4-west
 
-move(0,0,2,0)
+cnt = 0
 for x in tData:
-  print(x)
+  line = ''
+  for y in x:
+    if y == 0: char = '.'
+    else:
+      cnt += 1
+      if y in [3,5,7,11]: char = '#'
+      elif y == 3*5*7*11: char = '!'
+      else: char = 'X'
+    line = line + char
+  print(line)
+
+print("Part1: ", cnt)
