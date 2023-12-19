225fd9e45a17d0b471c461798fdd6e474fdfc540
Mihkel Tiganik <mtiganik@gmail.com>
2023-12-19 10:11:56 +0200

    part1 debug

--- day19/main.py
+++ day19/main.py
@@ -9,11 +9,10 @@ f = open("input.txt").read().splitlines()
 #s< : 7
 #s> : 8
 ratings = []
-workflow = ["px", [5,2006,"qkq",[4,2090,"A","rfg"]]],
-["pv",[5,1716,"R","A"]],
-["lnx", [3,1548,"A","A"]],
-["rfg",[8,537,"gd",[2,2440,"R","A"]]]
-workflows = []
+# workflow = ["px", [5,2006,"qkq",[4,2090,"A","rfg"]]],
+# ["pv",[5,1716,"R","A"]],
+# ["lnx", [3,1548,"A","A"]],
+# ["rfg",[8,537,"gd",[2,2440,"R","A"]]]
 
 def getWorkflowElement(str):
   cat = str[:2:]
@@ -29,11 +28,65 @@ def getWorkflow(str):
   name,cmd = str.split("{")
   return [name, getWorkflowElement(cmd)]
 
+def getComand(str):
+  str = str.replace("{","").replace("}","")
+  res = []
+  elems = str.split(",")
+  for x in elems:
+    val = x.split("=")[1]
+    res.append(int(val))
+  return res
 
+workflows = []
+cmnds = []
 endOfWorkFlows = False
+start =0 
 for x in f:
   if x == "":
     endOfWorkFlows = True
+    continue
   if not endOfWorkFlows:
     workflow = getWorkflow(x.replace("}",""))
-    print(workflow)
+    if workflow[0] == "in":
+      start = workflow
+    workflows.append(workflow)
+  else:
+    cmd = getComand(x)
+    cmnds.append(cmd)
+
+def getFlowById(flowId):
+  for flow in workflows:
+    if flow[0] == flowId: return flow
+  raise TypeError("shouldnt get here")
+
+cmdIdxs = ["x","m","a","s"]
+def getRatingResult(workflow,cmd):
+  valToCompare,comparator = workflow[1][0][0], workflow[1][0][1]
+  indexOfElem =  cmdIdxs.index(valToCompare)
+  if comparator == '<' and cmd[indexOfElem] < workflow[1][1]: inRating = True
+  elif comparator == '<' and cmd[indexOfElem] > workflow[1][1]: inRating = False
+  elif comparator == '>' and cmd[indexOfElem] > workflow[1][1]: inRating = True
+  elif comparator == '>' and cmd[indexOfElem] < workflow[1][1]: inRating = False
+
+  if inRating:
+    if workflow[1][2] == "A": return True
+    elif workflow[1][2] == "R": return False
+    else:
+      newWorkflow = getFlowById(workflow[1][2])
+      return getRatingResult(newWorkflow, cmd)
+  else:
+    if workflow[1][3] == "A": return True
+    elif workflow[1][3] == "R": return False
+    else:
+      falseWFlow = workflow[1][3]
+      if isinstance(falseWFlow, str):
+        newWorkflow = getFlowById(falseWFlow)
+      else: newWorkflow = getFlowById(workflow[1][3][0])
+      return getRatingResult(newWorkflow, cmd)
+
+
+    print("a")
+
+for cmd in cmnds:
+  isAccepted = getRatingResult(start, cmd)
+  print(isAccepted)
