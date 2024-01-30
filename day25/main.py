b6380cba14404dccfbea0d4d0d8896f610c99d18
Mihkel <mtiganik@gmail.com>
2024-01-30 13:43:17 +0200

    daily commit

--- day25/main.py
+++ day25/main.py
@@ -12,6 +12,6 @@ for x in f:
 for i in range(1,len(f)):
   cut_value, partition = nx.minimum_cut(G,nodes[0],nodes[i])
   if cut_value == 3:
-    print("Part 1:", len(partition[0])*len(partition[1]))
+    print("Part 1::", len(partition[0])*len(partition[1]))
     break
 
