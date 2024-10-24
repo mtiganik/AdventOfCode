a3fe08701c8c56fd5d3b980c92698fe248edcff8
Mihkel Tiganik <mtiganik@gmail.com>
2024-10-24 13:45:42 +0300

    part1

--- 2018/day18/main.py
+++ 2018/day18/main.py
@@ -4,34 +4,32 @@ for line in f:
   cmnds = [int(cmd) for cmd in line.split(" ")]
   break
 metaSum = 0
-i = 0
-dataStruck = []
-isNewNode = True
 
-#Base
-newNodes = cmnds[i]
-newMetaCnt = cmnds[i+1]
-dataStruck = [[] for x in range(newNodes)]
-newMeta = cmnds[-newMetaCnt:]
-cmnds = cmnds[:-newMetaCnt]
-metaSum += sum(newMeta)
-dataStruck.append(newMeta)  
+#start:
+stackList = []
+newNodes = cmnds.pop(0)
+newMetaCnt = cmnds.pop(0)
+stackList.append([newNodes, newMetaCnt])
 
-i = i+2
-#First child:
-newNodes = cmnds[i]
-newMetaCnt = cmnds[i+1]
-i = i+2
-if newNodes > 0:
-  newDataStruck = [[] for x in range(newNodes)]
-  newDataStruck.append(-newMetaCnt)
-  dataStruck[0] = newDataStruck
-else:
-  newMeta = cmnds[i:i+newMetaCnt]
-  metaSum += sum(newMeta)
-  dataStruck[0] = newMeta
-  i = i + newMetaCnt
+while len(cmnds) > 0:
+  mulBwrdsCheck = True
+  while mulBwrdsCheck:
+    lastElem = stackList[len(stackList) -1]
+    if lastElem[0] == 0:
+      metaSum += sum(cmnds[:lastElem[1]])
+      # cmnds = cmnds[:-lastElem[1]]
+      cmnds = cmnds[lastElem[1]:]
+      stackList.pop()
+    else:
+      lastElem[0] = lastElem[0] -1
+      mulBwrdsCheck = False
 
-print(dataStruck)
+  newNodes = cmnds.pop(0)
+  newMetaCnt = cmnds.pop(0)
+
+  if newNodes == 0:
+    metaSum += sum(cmnds[:newMetaCnt])
+    cmnds = cmnds[newMetaCnt:]
+  else:
+    stackList.append([newNodes, newMetaCnt])
 print(metaSum)
-# print(cmnds)
