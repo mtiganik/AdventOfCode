6fff8f6d78c3d9710c07f8da96d659e0f2442281
Mihkel Tiganik <mtiganik@gmail.com>
2024-12-03 11:38:37 +0200

    refactor d3

--- 2024/day03/main.py
+++ 2024/day03/main.py
@@ -1,11 +1,7 @@
 import re
-
-
 def f(x): return sum(int(a)*int(b) for a,b in re.findall("mul\((\d+),(\d+)\)", x))
 p1,p2 = f((s:=open("input.txt").read())),0
-while "don't()" in s:
-  ss = s.split("don't()",1)
-  s, p2 = ss[1][ss[1].find("do()"):], p2 + f(ss[0])
+while "don't()" in s: s, p2 = (ss := s.split("don't()", 1))[1][ss[1].find("do()"):], p2 + f(ss[0])
 
 print("Part 1: ",p1)
 print("Part 2: ",p2 + f(s))
