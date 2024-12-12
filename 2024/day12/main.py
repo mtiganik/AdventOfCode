18a1a3a618fcd0d94b6cdc69c3c70cff376ad333
Mihkel Tiganik <mtiganik@gmail.com>
2024-12-12 11:51:39 +0200

    refactoring

--- 2024/day12/main.py
+++ 2024/day12/main.py
@@ -1,31 +1,13 @@
 import copy
 
-grid = []
-for x in open("input.txt"):
-  k = list(x.strip())
-  k.insert(0,"*")
-  k.append('*')
-  grid.append(k)
-xLen = len(grid[0])
-yLen = len(grid)
-grid.insert(0,['*']*xLen)
-grid.append(['*']*xLen)
-xLen += 2
-yLen += 2
-
-dg = []
-dg = [[1000 for i in range(xLen) ] for j in range(yLen)]
+grid = [["*"] + list(x.strip()) + ["*"] for x in open("input.txt")]
+grid.insert(0,['*']*len(grid[0]))
+grid.append(['*']*len(grid))
+dg = [[1000 for i in range(len(grid[0])) ] for j in range(len(grid))]
 
 
 def findCurrPlantPerimeter(plant, ys,xs, p1,p2):
-
-  Area = 1
-  perimeter = 0
-  corners = 0
-  ci = 0
-  dg[ys][xs] = ci
-  currElStack = [[ys,xs]]
-  currAllCells = [[ys,xs]]
+  Area, perimeter,corners,ci,dg[ys][xs], currElStack,currAllCells = 1,0,0,0,0,[[ys,xs]],[[ys,xs]]
 
   def isSamePlant(ys,xs):
     nonlocal Area
@@ -39,24 +21,17 @@ def findCurrPlantPerimeter(plant, ys,xs, p1,p2):
         return 0
     return 1
 
-  def checkCorners(yc,xc, y1,x1,y2,x2):
-    if grid[yc][xc] == plant: 
-      if grid[y1][x1] != plant and grid[y2][x2] != plant:
-        return 1
-      return 0
-    else:
-      return int((grid[y1][x1] == plant) == (grid[y2][x2] == plant))
-    
+  def checkCorners(yc,xc, y1,x1,y2,x2): 
+    return int(grid[yc][xc] == plant and grid[y1][x1] != plant and grid[y2][x2] != plant) or int((grid[yc][xc] != plant) and (grid[y1][x1] == plant) == (grid[y2][x2] == plant))
+
   while True:
     ci += 1
     stackCpy = copy.deepcopy(currElStack)
     currElStack = []
-    for k in stackCpy:
-      y,x = k[0],k[1]
-      for dy,dx in [(-1,0),(0,1),(1,0),(0,-1)]:
-        perimeter += isSamePlant(y+dy,x+dx)
-      for dy,dx,y1,x1,y2,x2 in [(-1,1,-1,0,0,1),(1,1,0,1,1,0),(1,-1,0,-1,1,0),(-1,-1,-1,0,0,-1)]:
-        corners += checkCorners(y+dy,x+dx,y+y1,x+x1,y+y2,x+x2)
+    for [y,k] in stackCpy:
+      # y,x = k[0],k[1]
+      for dy,dx in [(-1,0),(0,1),(1,0),(0,-1)]: perimeter += isSamePlant(y+dy,x+dx)
+      for dy,dx,y1,x1,y2,x2 in [(-1,1,-1,0,0,1),(1,1,0,1,1,0),(1,-1,0,-1,1,0),(-1,-1,-1,0,0,-1)]:corners += checkCorners(y+dy,x+dx,y+y1,x+x1,y+y2,x+x2)
     if len(currElStack) == 0:
       for place in currAllCells:
         yi,xi = place[0], place[1]
