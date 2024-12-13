ed6c167089b3505987b624aa7d23f9eb28fd1448
Mihkel Tiganik <mtiganik@gmail.com>
2024-12-13 08:08:02 +0200

    p2 done

---
+++ 2024/day13/p2.py
@@ -0,0 +1,33 @@
+
+currx,curry = [],[]
+total = 0
+
+bnum = 10000000000000
+def calcResult(x,y):
+  A = (x[2]*y[1]-x[1]*y[2])/(x[0]*y[1]-x[1]*y[0])
+  B = (x[0]*y[2]-x[2]*y[0])/(x[0]*y[1]-x[1]*y[0])
+  if A.is_integer() and B.is_integer():
+    return 3*int(A) + int(B)
+  else:
+    return 0
+  print(A,B)
+for k in open("input.txt"):
+  if k.strip() == "":continue
+  k = k.replace("=","+")
+  f,k = k.split(": ")
+  arr = k.split(", ")
+  x = int(arr[0].split("+")[1])
+  y = int(arr[1].split("+")[1])
+  if("Prize" in f):
+    x = bnum + x
+    y = bnum + y
+    currx.append(x)
+    curry.append(y)
+    total += calcResult(currx,curry)
+    currx,curry = [],[]
+  else:
+    currx.append(x)
+    curry.append(y)
+
+
+print(total)
