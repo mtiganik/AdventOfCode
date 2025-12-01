6a9df068e6d298f304b42f63d383b2c43bc6aa94
Mihkel Tiganik <mtiganik@gmail.com>
2025-12-01 08:26:20 +0200

    p2 competed, refactored

--- 2025/day01/main.py
+++ 2025/day01/main.py
@@ -1,19 +1,31 @@
-currNum = 50
-count = 0
-pointer = ""
-for x in open("input.txt"):
-  pointer,newNum = x[0:1],int(x[1:])
-  # if newNum >100:
-  #   count += int(newNum/100)
-  #   newNum %=100
-  if pointer == "L":
-    newNum *=-1
-  
-  currNum += newNum
-  currNum %= 100
-  if not currNum:
-    count += 1
-  print("Current num:", currNum)
-  
+inp = "input.txt"
 
-print(count)
+def p1(f):
+    count,currNum = 0,50
+    for x in open(f):
+      pointer,newNum = x[0],int(x[1:])
+      if pointer == "L":
+        newNum *=-1
+      currNum += newNum
+      currNum %= 100
+      if not currNum:
+        count += 1
+    return count
+
+def p2(f):
+    count,currNum = 0,50
+    for x in open(f):
+        pointer,newNum = x[0],int(x[1:])
+        for i in range(newNum):
+            if pointer == "L":
+                currNum -= 1
+            else:
+                currNum += 1
+            currNum %= 100
+            if not currNum:
+                count += 1
+    return count
+
+
+print("Part 1", p1(inp))
+print("Part 2", p2(inp))
