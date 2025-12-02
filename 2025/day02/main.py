eadc7904ebb5d470299cd85e91cd17c83b1b24b5
Mihkel Tiganik <mtiganik@gmail.com>
2025-12-02 09:11:08 +0200

    part 2 completed

--- 2025/day02/main.py
+++ 2025/day02/main.py
@@ -1,13 +1,11 @@
-f = open("input.txt")
-count = 0
+file ="input.txt"
+p1Res, p2Res = 0,0
 
-def findInvalidIds(inp):
+def p1Calc(inp):
   res = 0
   if inp == "":
     return 0
   s,e = inp.split("-")
-  if len(e) - len(s) > 2:
-    raise Exception("too big number scale")
   if len(s) == len(e) and len(s) % 2 != 0:
     return 0
   if len(s) % 2 != 0 and len(e) %2 == 0:
@@ -15,22 +13,48 @@ def findInvalidIds(inp):
   if len(s) % 2 == 0 and len(e) %2 != 0:
     e = "9"*(len(s))
 
-  # split start to 2 elems
   halfSize = int(len(s) / 2)
-  # s1,s2 = s[0:halfSize], s[halfSize:]
-  # e1,e2 = e[0:halfSize], e[halfSize:]
-  # print(s1, " - ", s2)
   start, stop = int(s), int(e)
   for i in range(start, stop+1):
     i1,i2 = str(i)[0:halfSize], str(i)[halfSize:]
     if i1 == i2:
-      # print("valid: ", i1,i2)
       res += i
   return res
 
-for x in f:
+def p2Calc(inp):
+  res = 0
+  if inp == "":
+    return 0
+  s,e = inp.split("-")
+  if len(s) == 1:
+    print("manual check for ", inp, " add results from ", inp, " manually to p2 result")
+    return 0
+  halfSize = int(len(s) / 2)
+  start, stop = int(s), int(e)
+  #print(inp, ":")
+  for i in range(start, stop+1):
+    for n in range(1,int(len(e)+1 / 2)):
+      line = str(i)
+      arr = [line[i:i+n] for i in range(0, len(line),n)]
+      if arr.count(arr[0]) == len(arr):
+        resStr = "".join(arr)
+        #print(resStr)
+        res += int(resStr)
+        break
+  return res
+
+
+for x in open(file):
   elms = x.strip().split(",")
   for k in elms:
-    count += findInvalidIds(k)
+    p1Res += p1Calc(k)
 
-print("res:", count)
+print("Part 1:", p1Res)
+
+for x in open(file):
+  elms = x.strip().split(",")
+  for k in elms:
+    p2Res += p2Calc(k)
+
+
+print("part 2:", p2Res, " (plus manual add)" )
