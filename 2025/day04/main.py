f266e119a52efec0a56356e627b4fa8ef363f9a2
Mihkel Tiganik <mtiganik@gmail.com>
2025-12-04 09:25:22 +0200

    refactored version

--- 2025/day04/main.py
+++ 2025/day04/main.py
@@ -1,48 +1,22 @@
-f = open("input.txt")
-grid = []
-count = 0
-for x in f:
-  valToAdd = "." + x.strip() + "."
-  grid.append(list(valToAdd))
-
-firstLast = list("."*len(grid[0]))
-grid.insert(0,firstLast)
-grid.append(firstLast)
+input = "input.txt"
+p1,isP1,grid =0,True, [list("." + line.strip() + ".") for line in open(input)]
+border = list("."*len(grid[0])); grid = [border] + grid + [border]
 
 def isAccessable(i,j):
-  nearbyRolls = 0
-  places = [
-    [i-1,j-1],[i-1,j],[i-1,j+1], 
-    [i,j-1],[i,j+1],
-    [i+1,j-1],[i+1,j],[i+1,j+1]]
-  for k in places:
-    if grid[k[0]][k[1]] in ["@","x"]:
-      nearbyRolls += 1
-  if nearbyRolls < 4:
-    return True
-  return False
-for i in range(len(grid)):
-  for j in range(len(grid[0])):
-    if grid[i][j] == "@":
-      if isAccessable(i,j):
-        # print(i, "-", j, " returned true")
-        grid[i][j] = "x"
-        count += 1
-# for k in grid:
-#   print("".join(k))
-print(count)
+  cnt,neighbors=0,[[i-1,j-1],[i-1,j],[i-1,j+1],[i,j-1],[i,j+1],[i+1,j-1],[i+1,j],[i+1,j+1]]
+  for k in neighbors:
+    if grid[k[0]][k[1]] == "@":
+      cnt += 1
+  return True if cnt < 4 else False
 
+def execute(isP1,cnt = 0, ppt = []):
+  while True:
+    ppt = [[i,j] for i, row in enumerate(grid) for j, v in enumerate(row) if v == "@" and isAccessable(i, j)]
+    cnt += len(ppt)
+    if not ppt: return cnt
+    for i,j in ppt: grid[i][j] = "."; ppt = []
+    if isP1: return cnt
 
+print("Part1:", (p1 := execute(isP1= True, cnt = 0)))
+print("Part2:", execute(isP1=False, cnt = p1))
 
-#............
-#...xx.xx@x.. ..xx.xx@x.
-#.x@@.x.@.@@. x@@.@.@.@@
-#.@@@@@.x.@@. @@@@@.x.@@
-#.@.@@@@..@.. @.@@@@..@.
-#.@@.@@@@.@x. x@.@@@@.@x
-#..@@@@@@@.@. .@@@@@@@.@
-#..@.@.@.@@@. .@.@.@.@@@
-#.x.@@@.@@@@. x.@@@.@@@@
-#..@@@@@@@@.. .@@@@@@@@.
-#.x.x.@@@.x.. x.x.@@@.x. 
-#............
